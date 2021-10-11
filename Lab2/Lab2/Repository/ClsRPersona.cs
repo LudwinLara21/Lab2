b7ec45bd8c273eb3c61e4b552fc27e83229984ce
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2021-10-11 17:03:24 -0600

    Cambios-R

--- Lab2/Lab2/Repository/ClsRPersona.cs
+++ Lab2/Lab2/Repository/ClsRPersona.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using Lab2.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Lab2.Repository
 {
     class ClsRPersona
     {
+        public void Guardar(persona persona)
+        {
+            using (almacendepersonasEntities conexionBd = new almacendepersonasEntities())
+            {
+                try
+                {
+                    //Para insertar la informacion en la base de datos.
+                    conexionBd.persona.Add(persona);
+
+                    //Para guardar los cambios.
+                    conexionBd.SaveChanges();
+                    MessageBox.Show("Guardado");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error de base de datos" + ex);
+                }
+            }
+        }
+
+        public List<persona> ListOfDataOfTablePersona()
+        {
+            using (almacendepersonasEntities conexionBd = new almacendepersonasEntities())
+            {
+                List<persona> ListaOfData = conexionBd.persona.ToList(); // = Selcet * from estudiante
+                return ListaOfData;
+            }
+        }
     }
 }
